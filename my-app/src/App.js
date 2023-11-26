e4b32f82c55ea1c3b82af8e80a64f49091bea506
Craig Walton <craig.w@me.com>
2023-11-26 13:47:08 +0000

    Add more sliders, positioned and labelled.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Slider from './Slider';
 
 function App() {
-  return (
-    <div className="App">
-
-      <Slider />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Slider x={100} y={200} label={"Reflected by clouds"}/>
+            <Slider x={100} y={400} label={"Reflected by surface"}/>
+            <Slider x={300} y={300} label={"Absorbed by atmosphere"}/>
+        </div>
+    );
 }
 
 export default App;
--- my-app/src/Slider.js
+++ my-app/src/Slider.js
@@ -1,23 +1,24 @@
 import React, { useState } from 'react';
-import './Slider.css'; // Create this file for styling
+import './Slider.css';
 
-const Slider = () => {
-    const [value, setValue] = useState(50); // Initial value
+const Slider = ({ x, y, label }) => {
+    const [value, setValue] = useState(50);
 
     const handleSliderChange = (event) => {
         setValue(event.target.value);
     };
 
     return (
-        <div className="slider-container">
+        <div className="slider-container" style={{ position: 'absolute', left: x, top: y }}>
+            <label>{label}</label><br/>
             <input
                 type="range"
                 min="0"
                 max="100"
                 value={value}
                 onChange={handleSliderChange}
-            />
-            <p>Value: {value}</p>
+            /><br/>
+            <b>{value}</b>
         </div>
     );
 };
