fc8e9c333c083f7b133b2ba1fa868203d6557d94
Craig Walton <craig.w@me.com>
2023-12-08 21:11:54 +0000

    Do not render arrow when any parameter is null.

--- my-app/src/components/ShortwaveArrows.js
+++ my-app/src/components/ShortwaveArrows.js
@@ -1,8 +1,6 @@
 import './ShortwaveArrows.css';
 
 const ShortwaveArrows = ({x, y, scattered, reflected, atmosphere}) => {
-
-    const data = drawArrow(scattered, reflected, atmosphere);
     return (
         <div className="arrow-container" style={{position: 'absolute', left: x, top: y}}>
             <svg width="500" height="600" viewBox="-250 -100 500 600">
@@ -13,7 +11,7 @@ const ShortwaveArrows = ({x, y, scattered, reflected, atmosphere}) => {
                     </linearGradient>
                 </defs>
                 <path className={"shortwave-path"}
-                      d={data}/>
+                      d={drawArrow(scattered, reflected, atmosphere)}/>
                 <path className={"shortwave-path"}
                       d="M 0 -40
                          l 0 40
@@ -28,19 +26,22 @@ const ShortwaveArrows = ({x, y, scattered, reflected, atmosphere}) => {
     );
 };
 
-function drawArrow(scatteredValue, reflectedValue, atmosphereValue) {
-    const absorbedSurfaceValue = 100 - scatteredValue - reflectedValue - atmosphereValue;
+function drawArrow(scattered, reflected, atmosphere) {
+    if (scattered == null || reflected == null || atmosphere == null) {
+        return "";
+    }
+    const absorbedSurfaceValue = 100 - scattered - reflected - atmosphere;
     const scatteredRIn = 70;
-    const scatteredROut = scatteredRIn + scatteredValue;
+    const scatteredROut = scatteredRIn + scattered;
     const scatteredY = 60;
     const reflectedY = 400;
     const reflectedRIn = scatteredROut;
-    const reflectedROut = reflectedRIn + reflectedValue;
+    const reflectedROut = reflectedRIn + reflected;
     const absorbedSurfaceY = 400;
     const atmosphereX = 120;
     const atmosphereY = 200;
     const atmosphereROut = 50;
-    const atmosphereRIn = atmosphereROut - atmosphereValue;
+    const atmosphereRIn = atmosphereROut - atmosphere;
     const step = 5;
     let data = `
                M 0 0
@@ -48,7 +49,7 @@ function drawArrow(scatteredValue, reflectedValue, atmosphereValue) {
                a ${scatteredRIn} ${scatteredRIn} 0 0 1 ${-scatteredRIn * 2} 0
                l 0 ${-scatteredY}`;
     // Reflected arrow head.
-    const reflectedArrow = (scatteredValue + reflectedValue) / 2 + step;
+    const reflectedArrow = (scattered + reflected) / 2 + step;
     data += `
             l ${step} 0
             l ${-reflectedArrow} ${-reflectedArrow}
@@ -57,7 +58,7 @@ function drawArrow(scatteredValue, reflectedValue, atmosphereValue) {
     data += `
             l 0 ${reflectedY - reflectedROut}
             a ${reflectedROut} ${reflectedROut} 0 0 0 ${reflectedROut * 2} 0
-            L ${reflectedValue + scatteredValue} ${absorbedSurfaceY}`;
+            L ${reflected + scattered} ${absorbedSurfaceY}`;
     // Absorbed surface arrow head.
     const absorbedSurfaceArrow = (absorbedSurfaceValue) / 2 + step;
     data += `
@@ -66,11 +67,11 @@ function drawArrow(scatteredValue, reflectedValue, atmosphereValue) {
             l ${absorbedSurfaceArrow} ${-absorbedSurfaceArrow}
             l ${-step} 0`;
     data += `
-            L ${reflectedValue + scatteredValue + absorbedSurfaceValue} ${atmosphereY + atmosphereValue / 2 - atmosphereROut}
+            L ${reflected + scattered + absorbedSurfaceValue} ${atmosphereY + atmosphere / 2 - atmosphereROut}
             a ${atmosphereROut} ${atmosphereROut} 0 0 0 ${atmosphereROut} ${atmosphereROut}
-            L ${atmosphereX + 50} ${atmosphereY + atmosphereValue / 2}`;
+            L ${atmosphereX + 50} ${atmosphereY + atmosphere / 2}`;
     // Absorbed atmosphere arrow head.
-    const atmosphereArrow = (atmosphereValue) / 2 + step;
+    const atmosphereArrow = (atmosphere) / 2 + step;
     data += `
             l 0 ${step}
             l ${atmosphereArrow} ${-atmosphereArrow}
@@ -78,17 +79,17 @@ function drawArrow(scatteredValue, reflectedValue, atmosphereValue) {
             l 0 ${step}`;
     if (atmosphereRIn > 0) {
         data += `
-                L ${100 + atmosphereRIn} ${atmosphereY - atmosphereValue / 2}
+                L ${100 + atmosphereRIn} ${atmosphereY - atmosphere / 2}
                 a ${atmosphereRIn} ${atmosphereRIn} 0 0 1 ${-atmosphereRIn} ${-atmosphereRIn}`;
     } else {
-        data += `L 100 ${atmosphereY - atmosphereValue / 2}`;
+        data += `L 100 ${atmosphereY - atmosphere / 2}`;
     }
     data += `
             L 100 0
             Z`;
     // Inner / negative fill part.
     data += `
-            M ${scatteredValue} ${scatteredY}
+            M ${scattered} ${scatteredY}
             a ${scatteredROut} ${scatteredROut} 0 0 1 ${-scatteredROut * 2} 0
             l 0 ${reflectedY - reflectedROut - scatteredY}
             a ${reflectedRIn} ${reflectedRIn} 0 0 0 ${reflectedRIn * 2} 0
