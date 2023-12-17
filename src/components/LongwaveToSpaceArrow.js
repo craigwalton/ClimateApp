879c60a72a522615a2f6c36eac578b3f21c288bc
Craig Walton <craig.w@me.com>
2023-12-17 17:19:08 +0000

    Extract common liner gradient stops.

--- src/components/LongwaveToSpaceArrow.js
+++ src/components/LongwaveToSpaceArrow.js
@@ -10,8 +10,8 @@ const LongwaveToSpaceArrow = ({x, y, value}) => {
             <svg width="120" height="210" viewBox="-60 0 120 210" style={{position: "relative", left: -60}}>
                 <defs>
                     <linearGradient id="longwave-to-space-gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-                        <stop offset="0%" style={{stopColor: 'rgb(228,122,106)', stopOpacity: 1}}/>
-                        <stop offset="100%" style={{stopColor: 'rgb(197,141,128)', stopOpacity: 1}}/>
+                        <stop offset="0%" className={"longwave-gradient-light"}/>
+                        <stop offset="100%" className={"longwave-gradient-dark"}/>
                     </linearGradient>
                 </defs>
                 <path className={"longwave-to-space-path"}
